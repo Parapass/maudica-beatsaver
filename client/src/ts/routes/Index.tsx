cb67617c8abff1d966783204584ab07c7abae811
Para <parapasser@gmail.com>
2020-04-18 13:43:48 -0400

    Update Index.tsx

--- client/src/ts/routes/Index.tsx
+++ client/src/ts/routes/Index.tsx
@@ -3,16 +3,16 @@ import { ExtLink } from '../components/ExtLink'
 import { TextPage } from '../components/TextPage'
 
 export const Index: FunctionComponent = () => (
-  <TextPage title='BeatSaver'>
-    <p>Beat Saber's #1 unofficial beatmap distributor!</p>
+  <TextPage title='MAudica'>
+    <p>Audica's #1 unofficial beatmap distributor!</p>
     <hr />
 
     <p>
-      <b>Not everything is finished yet with the rewrite, please be patient.</b>
+      <b>Not everything is finished yet, please be patient.</b>
       <br />
       See{' '}
-      <ExtLink href='https://github.com/lolPants/beatsaver-reloaded/issues/1'>
-        this GitHub issue
+      <ExtLink href='https://github.com/MAudica/maudica-beatsaver'>
+        the GitHub repo
       </ExtLink>{' '}
       for more info.
     </p>
